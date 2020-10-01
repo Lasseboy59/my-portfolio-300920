93ff3a72a079d551a95d20d98311ff7a16de1004
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-10-01 12:20:23 +0300

    added Services and Title

--- src/constants/services.js
+++ src/constants/services.js
@@ -4,19 +4,19 @@ export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "ICT professional",
+    text: `I am an ICT professional having experience in different technical environments like software development / testing, telecommunications and microchip testing`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Coder / programmer",
+    text: `My work included Java programming (Java SE6, NetBeans, Git) as independent sprints or part of teams, software testing both manually and with optimized test cases, web site updates (HTML5, CSS) and customer support.`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Service Designer",
+    text: `I developed Help Desk operations for the access control- and video surveillance systems- product lines by modelling the system platform and to assist the implementation of the chosen system.`,
   },
 ]
