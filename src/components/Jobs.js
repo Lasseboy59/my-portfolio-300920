20483237144fa108e08befde5727675d92895d02
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-10-02 12:53:38 +0300

    Jobs comp. toggle through jobs (clog)

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -21,7 +21,33 @@ const query = graphql`
 `
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query);
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, desc } = jobs[value];
+  console.log(company, position, date, desc)
+
+  return <section className="section jobs">
+    <Title title="experience" />
+    <div className="jobs-center">
+      <div className="btn-container">
+        {jobs.map((item, index) => {
+          return (
+            <button
+              key={item.strapiId}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index === value &&
+                'active-button'}`}
+            >
+              {item.company}
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  </section>
 }
 
 export default Jobs
